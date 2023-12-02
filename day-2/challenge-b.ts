d9364c823c2e40d261e8b2e02dab713e79ee5965
Julien Blégean <julien@kompose.co>
2023-12-02 18:52:30 +0100

    ✨ day 2

---
+++ day-2/challenge-b.ts
@@ -0,0 +1,84 @@
+const LIMITS = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+const COLORS = Object.keys(LIMITS);
+
+/**
+ * parse string data to a pretty object with this shape :
+{
+  id: 5,
+  draws: [
+    {
+      red: 6,
+      blue: 1,
+      green: 3
+    }, {
+      blue: 2,
+      red: 1,
+      green: 2
+    }
+  ]
+}
+ */
+const parseLine = (line: string) => {
+  const regexGroups = /Game ([0-9]+): (.*)/g.exec(line);
+
+  const id = parseInt(regexGroups[1]);
+
+  const sets = regexGroups[2].split("; ");
+
+  const draws = [];
+  sets.forEach((set: string) => {
+    const items = set.split(", ");
+
+    const draw = {};
+    items.forEach((item: string) => {
+      item = item.split(" ");
+      const amount = parseInt(item[0]);
+      const color = item[1];
+
+      draw[color] = amount;
+    });
+    draws.push(draw);
+  });
+
+  return {
+    id,
+    draws,
+  };
+};
+
+export const run = async (input: string) => {
+  const lines = input.split("\n");
+
+  let total = 0;
+
+  // each game
+  lines.forEach((line: string) => {
+    const data = parseLine(line);
+
+    // keep maximum of each color for this game
+    const maximums = {};
+    for (let i = 0; i < data.draws.length; i++) {
+      const draw = data.draws[i];
+
+      for (let j = 0; j < COLORS.length; j++) {
+        const color = COLORS[j];
+        maximums[color] = Math.max(maximums[color] || 0, draw[color] || 1);
+      }
+    }
+
+    // compute power
+    let power = 1;
+    for (let j = 0; j < COLORS.length; j++) {
+      power *= maximums[COLORS[j]];
+    }
+
+    total += power;
+  });
+
+  return total;
+};
